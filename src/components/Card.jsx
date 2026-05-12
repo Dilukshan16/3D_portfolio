cf30deead01c19416d20ccec3a5e24830d3f1304
Dilukshan <pdiluksan@gmail.com>
2026-05-13 01:01:09 +0530

    about section implementation complete

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,25 @@
+import { motion } from 'framer-motion'
 import { img } from 'motion/react-m'
 import React from 'react'
 
-const Card = ({style, text, image}) => {
+const Card = ({style, text, image, containerRef}) => {
   return image && !text ? (
-    <img src={image} alt="" className='absolute w-15 cursor-grab' style={style}/>):(
-    <div className='absolute px-1 py-4 text-xl text-center 
+    <motion.img src={image} alt="" className='absolute w-15 cursor-grab' style={style}
+    whileHover={{scale:1.05 }}
+    drag
+    dragConstraints={containerRef}
+    dragElastic={1}
+    />):(
+    <motion.div className='absolute px-1 py-4 text-xl text-center 
     rounded-full ring ring-gray-700 font-extralight bg-storm w-[12rem] cursor-grab'
-    style={style}>
+    style={style}
+    whileHover={{scale:1.05 }}
+    drag
+    dragConstraints={containerRef}
+    dragElastic={1}
+    >
       {text}
-    </div>
+    </motion.div>
   )
 }
 
